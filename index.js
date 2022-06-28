25df0813460aec463d1bf7f2f448e63986718205
Gabriel Boudreaux <boudreauxgabriel1@gmail.com>
2022-06-28 16:43:03 -0500

    commit tree b68133a7e3083a9209d5114d6ec8f6f4cdf16a9c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (blockNumber) {
+    if (blockNumber > 42) {
+      return blockNumber - 42;
+    } else {
+      return 42 - blockNumber;
+    }
+  }
+
+  function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+  }
+
+  function distanceTravelledInFeet(initial,final) {
+    if (initial < final) {
+        return (final - initial) * 264;
+        
+    }
+     else {
+        return (initial - final) * 264;
+    }
+    
+  }
+  function calculatesFarePrice(initial,final) {
+    const distance = distanceTravelledInFeet(initial,final);
+    if (distance <= 400) { 
+        return 0;
+        
+    } else if(distance > 400 && distance <= 2000) {
+        return 2.56;
+    } else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else {
+        return 'cannot travel that far';
+    }
+    
+  }
